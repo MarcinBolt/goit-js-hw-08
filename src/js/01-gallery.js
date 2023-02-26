933ae183008b07d64332773efad2d7567676bbd7
MarcinBolt <marcin.boltruczyk@gmail.com>
2023-02-26 22:03:39 +0100

    solved task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryDiv = document.querySelector('div.gallery');
+const myGallery = galleryItems
+  .map(
+    image => `<a class="gallery__item" href="${image.original}">
+  <img
+    class="gallery__image"
+    src="${image.preview}"
+    alt="${image.description}"
+  />
+</a>`
+  )
+  .join('');
+
+galleryDiv.insertAdjacentHTML('afterbegin', myGallery);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+});
